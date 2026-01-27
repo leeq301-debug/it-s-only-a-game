7e1b4d08a51cd9e45ae5e3347007068563db8f5e
leeq301-debug <leeq301@gmail.com>
2026-01-27 11:32:26 +0000

    r

--- scrip2.js
+++ scrip2.js
@@ -1,14 +1,5 @@
-import {hinhanh,trinhchieu} from './myscrip.js';
 function datlaixiu() {
     document.getElementById("daura").innerHTML = "";
     document.getElementById("nuttai").checked = false;
     document.getElementById("nutxiu").checked = false;
-    if (hinhanh.complete){
-        trinhchieu(1,1);
-    }
-    else {
-        hinhanh.onload = function(){
-            trinhchieu(1,1);
-        }
-    }
 }
